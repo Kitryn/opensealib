03d8b1dc8e122e45708dc9cbbf00d6b67581451d
Kitryn <snowfennard@gmail.com>
2021-03-21 22:52:36 +0800

    Typescript config changes - implemented postbuild scripts, added correct gql files

---
+++ tools/build.ts
@@ -0,0 +1,11 @@
+// Stolen from https://stackoverflow.com/questions/56902517/how-can-i-copy-my-static-files-to-my-output-location-in-a-typescript-express-pro
+import shell from 'shelljs'
+
+const buildFolder = './dist/'
+
+const folders = new Set(['./src/gql'])
+
+// Copy Folders
+folders.forEach((folder) => {
+    shell.cp('-R', folder, buildFolder)
+})
