913c6a2f72f0a2d6ed6f6afe2fec378dac2ccf85
Kitryn <snowfennard@gmail.com>
2021-04-05 01:48:43 +0800

    NEEDS REFINING --> Added POC concept of fetching trait data from blockchain for crypteriors

--- src/csvGenerator.ts
+++ src/csvGenerator.ts
@@ -5,9 +5,12 @@ const logger = parentLogger.child({module: 'csvGenerator'})
 import { ContractAddress, CollectionSlug, Asset, ApiError, GqlApiError, ValidateResponseError } from './types'
 import { OpenSeaLib } from './opensealib'
 import PQueue from 'p-queue'
+import fetch from 'node-fetch'
 import jsonexport from 'jsonexport'
+import {Web3Interface} from './web3interface'
 
 const CONCURRENCY = 400
+let web3interface: Web3Interface
 
 function flattenTraits(asset: Asset): any {
     let output: any = {...asset}
@@ -66,4 +69,53 @@ export async function generateCsv(address: ContractAddress, collection: Collecti
     })
 
     return await jsonexport(parsedOutput)
+}
+
+async function generateCsvFromUri(keys: any[]) {
+    if (web3interface == null) {
+        web3interface = new Web3Interface(keys)  // lazy loading
+    }
+
+    const queue = new PQueue({concurrency: CONCURRENCY})
+
+    const totalSupply = await web3interface.crypteriorsInstance.methods.totalSupply().call()
+    let count = 0
+    queue.on('active', () => {
+        count += 1
+        if (count % 100 === 0) logger.info(`Item ${count}. Size ${queue.size} Pending ${queue.pending}`)
+    })
+
+    let output: any[] = []
+    for (let i = 0; i < totalSupply; i++) {
+        (async () => {
+            const uri = `https://gateway.pinata.cloud/ipfs/QmdcoQDb6sNgkkChUT6HJ6voS7XnDot2u6ZQwZTSBM3uAy/${i}`
+            
+            let json = await queue.add(() => {
+                return fetch(uri, {
+                    method: 'GET'
+                }).then(res => res.json()).catch(err => {
+                    console.error(err)
+                })
+            })
+
+            if (json) output.push(json)
+        })()
+    }
+
+    await queue.onIdle()  // all work is done
+    output = output.sort((a, b) => {
+        const an = parseInt(a.name.split('#')[1])
+        const bn = parseInt(b.name.split('#')[1])
+        return an - bn
+    }).map((elem) => {
+        let output: any = {
+            tokenId: parseInt(elem.name.split('#')[1])
+        }
+        for (let trait of elem.attributes) {
+            output[trait.trait_type] = trait.value
+        }
+        return output
+    })
+    logger.info(`${output.length} / ${totalSupply} fetched`)
+    return await jsonexport(output)
 }
